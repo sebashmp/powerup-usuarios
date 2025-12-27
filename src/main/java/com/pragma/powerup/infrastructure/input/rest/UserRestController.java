c94dda42ca9761258e6e28b168154a920ef71028
sebashmp <sebastian.hidalgo@pragma.com.co>
2025-12-26 19:44:22 -0500

    feat: add security configuration and enhance owner creation API documentation

--- src/main/java/com/pragma/powerup/infrastructure/input/rest/UserRestController.java
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/UserRestController.java
@@ -2,6 +2,10 @@ package com.pragma.powerup.infrastructure.input.rest;
 
 import com.pragma.powerup.application.dto.request.UserRequestDto;
 import com.pragma.powerup.application.handler.IUserHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +23,12 @@ public class UserRestController {
 
     private final IUserHandler userHandler;
 
+    @Operation(summary = "Add a new owner")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Owner created", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Invalid input data", content = @Content),
+            @ApiResponse(responseCode = "409", description = "User already exists", content = @Content)
+    })
     @PostMapping("/owner")
     public ResponseEntity<Void> createOwner(@Valid @RequestBody UserRequestDto userRequestDto) {
         userHandler.saveOwner(userRequestDto);
