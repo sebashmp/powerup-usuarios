1838ec2f3971a3baa250e04d19dc0e6c108ff450
sebashmp <sebastian.hidalgo@pragma.com.co>
2025-12-29 15:46:23 -0500

    feat: add user retrieval functionality with response mapping

---
+++ src/main/java/com/pragma/powerup/application/mapper/IUserResponseMapper.java
@@ -0,0 +1,13 @@
+package com.pragma.powerup.application.mapper;
+
+import com.pragma.powerup.application.dto.response.UserResponseDto;
+import com.pragma.powerup.domain.model.UserModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface IUserResponseMapper {
+    UserResponseDto toResponse(UserModel userModel);
+}
